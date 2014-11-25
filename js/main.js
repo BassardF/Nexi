63c262ed2cfd25815a09711282ccf137ccb1f92e
frank bassard <f.bassard@gmail.com>
2014-11-25 22:21:06 +1300

    Boundaries & link drawing.

--- js/main.js
+++ js/main.js
@@ -41,25 +41,93 @@ CHAT = {
 
 // RaphaelJS
 
+Raphael.fn.connection = function (obj1, obj2, line, bg) {
+  if (obj1.line && obj1.from && obj1.to) {
+    line = obj1;
+    obj1 = line.from;
+    obj2 = line.to;
+  }
+  var bb1 = obj1.getBBox(),
+  bb2 = obj2.getBBox(),
+  p = [{x: bb1.x + bb1.width / 2, y: bb1.y - 1},
+  {x: bb1.x + bb1.width / 2, y: bb1.y + bb1.height + 1},
+  {x: bb1.x - 1, y: bb1.y + bb1.height / 2},
+  {x: bb1.x + bb1.width + 1, y: bb1.y + bb1.height / 2},
+  {x: bb2.x + bb2.width / 2, y: bb2.y - 1},
+  {x: bb2.x + bb2.width / 2, y: bb2.y + bb2.height + 1},
+  {x: bb2.x - 1, y: bb2.y + bb2.height / 2},
+  {x: bb2.x + bb2.width + 1, y: bb2.y + bb2.height / 2}],
+  d = {}, dis = [];
+  for (var i = 0; i < 4; i++) {
+    for (var j = 4; j < 8; j++) {
+      var dx = Math.abs(p[i].x - p[j].x),
+      dy = Math.abs(p[i].y - p[j].y);
+      if ((i == j - 4) || (((i != 3 && j != 6) || p[i].x < p[j].x) && ((i != 2 && j != 7) || p[i].x > p[j].x) && ((i != 0 && j != 5) || p[i].y > p[j].y) && ((i != 1 && j != 4) || p[i].y < p[j].y))) {
+        dis.push(dx + dy);
+        d[dis[dis.length - 1]] = [i, j];
+      }
+    }
+  }
+  if (dis.length == 0) {
+    var res = [0, 4];
+  } else {
+    res = d[Math.min.apply(Math, dis)];
+  }
+  var x1 = p[res[0]].x,
+  y1 = p[res[0]].y,
+  x4 = p[res[1]].x,
+  y4 = p[res[1]].y;
+  dx = Math.max(Math.abs(x1 - x4) / 2, 10);
+  dy = Math.max(Math.abs(y1 - y4) / 2, 10);
+  var x2 = [x1, x1, x1 - dx, x1 + dx][res[0]].toFixed(3),
+  y2 = [y1 - dy, y1 + dy, y1, y1][res[0]].toFixed(3),
+  x3 = [0, 0, 0, 0, x4, x4, x4 - dx, x4 + dx][res[1]].toFixed(3),
+  y3 = [0, 0, 0, 0, y1 + dy, y1 - dy, y4, y4][res[1]].toFixed(3);
+  var path = ["M", x1.toFixed(3), y1.toFixed(3), "C", x2, y2, x3, y3, x4.toFixed(3), y4.toFixed(3)].join(",");
+  if (line && line.line) {
+    line.bg && line.bg.attr({path: path});
+    line.line.attr({path: path});
+  } else {
+    var color = typeof line == "string" ? line : "#000";
+    return {
+      bg: bg && bg.split && this.path(path).attr({stroke: bg.split("|")[0], fill: "none", "stroke-width": bg.split("|")[1] || 3}),
+      line: this.path(path).attr({stroke: color, fill: "none"}),
+      from: obj1,
+      to: obj2
+    };
+  }
+};
+
 Raphael.st.draggable = function() {
-  var me = this, lx = 0, ly = 0,ox = 0, oy = 0,
+  var me = this,
+  lx = 0,
+  ly = 0,
+  ox = 0,
+  oy = 0,
   moveFnc = function(dx, dy) {
     lx = dx + ox;
     ly = dy + oy;
     me.transform('t' + lx + ',' + ly);
+    for (var i = DRAW.tmp.connections.length; i--;) {
+      DRAW.tmp.paper.connection(DRAW.tmp.connections[i]);
+    }
+    DRAW.tmp.paper.safari();
   },
-  startFnc = function() {},
+  startFnc = function() {
+    this.ox = this.type == "rect" ? this.attr("x") : this.attr("cx");
+    this.oy = this.type == "rect" ? this.attr("y") : this.attr("cy");
+  },
   endFnc = function() {
     ox = lx;
     oy = ly;
   };
-
   this.drag(moveFnc, startFnc, endFnc);
 };
 
 var DRAW = {
   tmp : {
-    nodes : []
+    nodes : [],
+    connections : []
   },
 
   init : function(){
@@ -117,7 +185,7 @@ var DRAW = {
 
   bubble : function(x, y, content){
     // Text
-    var text = DRAW.tmp.paper.text(x, y, content).attr({"font-size" : 13, "fill" : "#333333"}),
+    var text = DRAW.tmp.paper.text(x, y, content).attr({"font-size" : 13, "fill" : "#333333",  cursor: "move"}),
         box = text.getBBox();
 
     // Bubble
@@ -126,13 +194,47 @@ var DRAW = {
                   yrad : box.width + 5,
                   xrad : box.height + 5};
 
-    var ellipse = DRAW.tmp.paper.ellipse(metrics.xcenter, metrics.ycenter, metrics.yrad, metrics.xrad).attr({"stroke" : "#e3e3e3", "fill" : "#f2f2f2"}).toBack();
+    var ellipse = DRAW.tmp.paper.ellipse(metrics.xcenter, metrics.ycenter, metrics.yrad, metrics.xrad).attr({"stroke" : "#e3e3e3", "fill" : "#f2f2f2", cursor: "move"}).toBack();
 
     var nodeSet = DRAW.tmp.paper.set();
     nodeSet.push(text);
     nodeSet.push(ellipse);
     nodeSet.draggable();
     DRAW.tmp.nodes.push(nodeSet);
+
+    ellipse.onDragOver(function(target){
+      DRAW.addConnection(this, target);
+    });
+
+    text.onDragOver(function(target){
+      DRAW.addConnection(this, target);
+    });
+
+  },
+
+  addConnection : function(shape1, shape2){
+    if (shape2.type !== "ellipse" || (shape1.type !== "ellipse" && shape1.type !== "text")){
+      return ;
+    }
+    if(shape1.type == "text"){
+      for (var i = DRAW.tmp.nodes.length; i--;){
+        if(DRAW.tmp.nodes[i].items[0].id === shape1.id){
+          console.log("swap");
+          shape1 = DRAW.tmp.nodes[i].items[1];
+        }
+        if(shape1.id === shape2.id){
+          return ;
+        }
+      }
+    }
+    for (var i = DRAW.tmp.connections.length; i--;) {
+      if((shape1.id == DRAW.tmp.connections[i].from.id && shape2.id == DRAW.tmp.connections[i].to.id) || (shape1.id == DRAW.tmp.connections[i].to.id && shape2.id == DRAW.tmp.connections[i].from.id)){
+        return;
+      }
+    }
+    console.log(shape1.type + " " + shape2.type + " ok");
+    DRAW.tmp.connections.push(DRAW.tmp.paper.connection(shape1, shape2, "#e3e3e3"));
+
   }
 }
 
