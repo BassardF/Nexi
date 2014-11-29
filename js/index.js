11c77176e3ca901c45f001e543a0d33e4eae9d88
frank bassard <f.bassard@gmail.com>
2014-11-29 17:42:52 +1300

    Alpha index.

--- js/index.js
+++ js/index.js
@@ -13,19 +13,21 @@ var DRAW = {
 
   logo : function(){
     DRAW.tmp.logo = DRAW.tmp.paper.image("img/logo-white.png", DRAW.tmp.width / 2 - 150, DRAW.tmp.height / 4 - 60, 300, 120).toFront();
+    DRAW.tmp.subLogo = DRAW.tmp.paper.text(DRAW.tmp.width / 2 - 150 + 150, DRAW.tmp.height / 4 - 60 + 150, "Nurse your ideas").attr({"fill" : "#fcfcfc", "font-size" : 18}).toFront();
   },
 
   center : function(x){
     var color = "#FB595D";
     DRAW.tmp.paper.circle(DRAW.tmp.width / 2, DRAW.tmp.height / 2, 20 + x).attr({"fill" : color, "stroke" : color});
     DRAW.tmp.logo.toFront();
+    DRAW.tmp.subLogo.toFront();
   },
 
   circle : function(){
-    if(DRAW.tmp.growth <= DRAW.tmp.width / 2){
+    if(DRAW.tmp.growth <= DRAW.tmp.width / 4){
       setTimeout(function(){
         DRAW.circle();
-      }, 50);
+      }, 15);
     } else{
       setTimeout(function(){
         DRAW.endAnim();
@@ -43,7 +45,7 @@ var DRAW = {
       opacity : 1
     }, 1000, "linear", function(){
       DRAW.center(DRAW.tmp.growth);
-      DRAW.tmp.growth += 20;
+      DRAW.tmp.growth += 10;
     });
   },
 
@@ -53,9 +55,9 @@ var DRAW = {
   },
 
   tryButton : function(){
-    var x = DRAW.tmp.width / 2 - 100,
+    var x = DRAW.tmp.width / 2 - 150,
         y = DRAW.tmp.height / 2 + DRAW.tmp.height / 6 - 30,
-        wd = 200,
+        wd = 300,
         ht = 60,
         rect = DRAW.tmp.paper.rect(x, y, wd, ht, 5).attr({
           "stroke" : "#fcfcfc",
@@ -65,7 +67,7 @@ var DRAW = {
         }).animate({
           opacity : 1
         }, 500, ">", function(){}),
-        text = DRAW.tmp.paper.text(x + wd / 2, y + ht / 2, "Try").attr({
+        text = DRAW.tmp.paper.text(x + wd / 2, y + ht / 2, "Start").attr({
           "fill" : "#fcfcfc",
           "font-size" : "18",
           "opacity" : 0,
@@ -97,9 +99,9 @@ var DRAW = {
   },
 
   quidButton : function(){
-    var x = DRAW.tmp.width / 2 - 100,
+    var x = DRAW.tmp.width / 2 - 150,
     y = DRAW.tmp.height / 2 + DRAW.tmp.height / 6 - 30 + 80,
-    wd = 200,
+    wd = 300,
     ht = 60,
     rect = DRAW.tmp.paper.rect(x, y, wd, ht, 5).attr({
       "stroke" : "#fcfcfc",
@@ -153,49 +155,45 @@ var DRAW = {
     DRAW.tmp.quidButton.text.animate({"opacity" : 0}, 150, ">", function(){
       DRAW.tmp.quidButton.text.remove();
       DRAW.tmp.quidButton.rect.remove();
-      DRAW.tmp.tryButton.rect.animate({"y" : DRAW.tmp.height - DRAW.tmp.tryButton.rect.attr("height") - 30}, 150, ">", function(){
+      //DRAW.tmp.tryButton.text.animate({"y" : DRAW.tmp.height - DRAW.tmp.tryButton.rect.attr("height") / 2 - 30}, 150, ">");
+      //DRAW.tmp.tryButton.rect.animate({"y" : DRAW.tmp.height - DRAW.tmp.tryButton.rect.attr("height") - 30}, 150, ">", function(){
+
+      DRAW.tmp.tryButton.text.animate({"y" : DRAW.tmp.height - DRAW.tmp.tryButton.rect.attr("height") / 2 - 15}, 150, ">");
+      DRAW.tmp.tryButton.rect.animate({"y" : DRAW.tmp.height - DRAW.tmp.tryButton.rect.attr("height") - 15}, 150, ">", function(){
         DRAW.whiteBox();
       });
-      DRAW.tmp.tryButton.text.animate({"y" : DRAW.tmp.height - DRAW.tmp.tryButton.rect.attr("height") / 2 - 30}, ">");
     });
 
     var x = DRAW.tmp.logo.attr(x);
     DRAW.tmp.logo.animate({"opacity" : 0}, 150, ">", function(){DRAW.tmp.logo.remove()});
+    DRAW.tmp.subLogo.animate({"opacity" : 0}, 150, ">", function(){DRAW.tmp.logo.remove()});
   },
 
   whiteBox : function(){
-    var height = DRAW.tmp.tryButton.rect.attr("y") - 30;
-    console.log(height);
-    DRAW.tmp.whiteBox = DRAW.tmp.paper.rect(0, 0, DRAW.tmp.width, 0).attr({"stroke" : "#ddd", "fill" : "#fcfcfc"}).animate({"height" : height}, 150, ">", function(){DRAW.tips()}).toFront();
+    var height = DRAW.tmp.tryButton.rect.attr("y") - 15;
+    DRAW.tmp.whiteBox = DRAW.tmp.paper.rect(0, 0, DRAW.tmp.width, 0).attr({"stroke" : "#fcfcfc", "fill" : "#fcfcfc"}).animate({"height" : height}, 150, ">", function(){DRAW.tips()}).toFront();
   },
 
   tips : function(){
     var height = DRAW.tmp.whiteBox.attr("height");
 
-    // left
-    var left = {
-      h : height / 2,
-      w : height / 2 * 477 / 287
-    };
-    DRAW.tmp.teftTitle = DRAW.tmp.logo = DRAW.tmp.paper.image("img/logo-pink.png", 15, 15, 150, 60).toFront();
-    DRAW.tmp.leftTips = DRAW.tmp.paper.image("img/sample-mapper.png", DRAW.tmp.width / 4 - left.w / 2 , height / 2 - left.h / 2, left.w, left.h).attr({"opacity" : 0}).animate({"opacity" : 1}, 500, ">").toFront();
-
     // right
-    DRAW.tmp.paper.circle(DRAW.tmp.width / 2 + 30, height / 5, 30).attr({"opacity" : 0, "fill" : DRAW.tmp.colors[0], "stroke" : DRAW.tmp.colors[0]}).animate({"opacity" : 1}, 500, ">").toFront();
-    DRAW.tmp.paper.image("img/png/bulb-white.png", DRAW.tmp.width / 2 + 30 - 15, height / 5 - 15, 30, 30).attr({"opacity" : 0}).animate({"opacity" : 1}, 500, ">").toFront();
-    DRAW.tmp.paper.text(DRAW.tmp.width / 2 + 30 + 40, height / 5, "Boost your creativity").attr({"opactity" : 0, "font-size" : "16", "text-anchor" : "start", "fill" : "#222222"}).animate({"opacity" : 1}, 500, ">").toFront();
+    DRAW.tmp.paper.circle(60, height / 5, 30).attr({"opacity" : 0, "fill" : DRAW.tmp.colors[0], "stroke" : DRAW.tmp.colors[0]}).animate({"opacity" : 1}, 500, ">").toFront();
+    DRAW.tmp.paper.rect(0, height / 5 - 30, 4, 60).attr({"opacity" : 0, "fill" : DRAW.tmp.colors[0], "stroke" : DRAW.tmp.colors[0]}).animate({"opacity" : 1}, 500, ">").toFront();
+    DRAW.tmp.paper.image("img/png/bulb-white.png", 60 - 15, height / 5 - 15, 30, 30).attr({"opacity" : 0}).animate({"opacity" : 1}, 500, ">").toFront();
+    DRAW.tmp.paper.text(60 + 40, height / 5, "Boost your creativity").attr({"opactity" : 0, "font-size" : "18", "text-anchor" : "start", "fill" : "#222222"}).animate({"opacity" : 1}, 500, ">").toFront();
 
-    DRAW.tmp.paper.circle(DRAW.tmp.width / 2 + 30, 2 * height / 5, 30).attr({"opacity" : 0, "fill" : DRAW.tmp.colors[1], "stroke" : DRAW.tmp.colors[1]}).animate({"opacity" : 1}, 500, ">").toFront();
-    DRAW.tmp.paper.image("img/png/atomic-white.png", DRAW.tmp.width / 2 + 30 - 15, 2 * height / 5 - 15, 30, 30).attr({"opacity" : 0}).animate({"opacity" : 1}, 500, ">").toFront();
-    DRAW.tmp.paper.text(DRAW.tmp.width / 2 + 30 + 40, 2 * height / 5, "Structure your thoughts").attr({"opactity" : 0, "font-size" : "16", "text-anchor" : "start", "fill" : "#222222"}).animate({"opacity" : 1}, 500, ">").toFront();
+    DRAW.tmp.paper.circle(60, 2 * height / 5, 30).attr({"opacity" : 0, "fill" : DRAW.tmp.colors[1], "stroke" : DRAW.tmp.colors[1]}).animate({"opacity" : 1}, 500, ">").toFront();
+    DRAW.tmp.paper.image("img/png/atomic-white.png", 60 - 15, 2 * height / 5 - 15, 30, 30).attr({"opacity" : 0}).animate({"opacity" : 1}, 500, ">").toFront();
+    DRAW.tmp.paper.text(60 + 40, 2 * height / 5, "Structure your thoughts").attr({"opactity" : 0, "font-size" : "18", "text-anchor" : "start", "fill" : "#222222"}).animate({"opacity" : 1}, 500, ">").toFront();
 
-    DRAW.tmp.paper.circle(DRAW.tmp.width / 2 + 30, 3 * height / 5, 30).attr({"opacity" : 0, "fill" : DRAW.tmp.colors[2], "stroke" : DRAW.tmp.colors[2]}).animate({"opacity" : 1}, 500, ">").toFront();
-    DRAW.tmp.paper.image("img/png/users-white.png", DRAW.tmp.width / 2 + 30 - 15, 3 * height / 5 - 15, 30, 30).attr({"opacity" : 0}).animate({"opacity" : 1}, 500, ">").toFront();
-    DRAW.tmp.paper.text(DRAW.tmp.width / 2 + 30 + 40, 3 * height / 5, "Share with your team").attr({"opactity" : 0, "font-size" : "16", "text-anchor" : "start", "fill" : "#222222"}).animate({"opacity" : 1}, 500, ">").toFront();
+    DRAW.tmp.paper.circle(60, 3 * height / 5, 30).attr({"opacity" : 0, "fill" : DRAW.tmp.colors[2], "stroke" : DRAW.tmp.colors[2]}).animate({"opacity" : 1}, 500, ">").toFront();
+    DRAW.tmp.paper.image("img/png/users-white.png", 60 - 15, 3 * height / 5 - 15, 30, 30).attr({"opacity" : 0}).animate({"opacity" : 1}, 500, ">").toFront();
+    DRAW.tmp.paper.text(60 + 40, 3 * height / 5, "Share with your team").attr({"opactity" : 0, "font-size" : "18", "text-anchor" : "start", "fill" : "#222222"}).animate({"opacity" : 1}, 500, ">").toFront();
 
-    DRAW.tmp.paper.circle(DRAW.tmp.width / 2 + 30, 4 * height / 5, 30).attr({"opacity" : 0, "fill" : DRAW.tmp.colors[3], "stroke" : DRAW.tmp.colors[3]}).animate({"opacity" : 1}, 500, ">").toFront();
-    DRAW.tmp.paper.image("img/png/hand-white.png", DRAW.tmp.width / 2 + 30 - 15, 4 * height / 5 - 15, 30, 30).attr({"opacity" : 0}).animate({"opacity" : 1}, 500, ">").toFront();
-    DRAW.tmp.paper.text(DRAW.tmp.width / 2 + 30 + 40, 4 * height / 5, "Free of charge").attr({"opactity" : 0, "font-size" : "16", "text-anchor" : "start", "fill" : "#222222"}).animate({"opacity" : 1}, 500, ">").toFront();
+    DRAW.tmp.paper.circle(60, 4 * height / 5, 30).attr({"opacity" : 0, "fill" : DRAW.tmp.colors[3], "stroke" : DRAW.tmp.colors[3]}).animate({"opacity" : 1}, 500, ">").toFront();
+    DRAW.tmp.paper.image("img/png/hand-white.png", 60 - 15, 4 * height / 5 - 15, 30, 30).attr({"opacity" : 0}).animate({"opacity" : 1}, 500, ">").toFront();
+    DRAW.tmp.paper.text(60 + 40, 4 * height / 5, "Free of charge").attr({"opactity" : 0, "font-size" : "18", "text-anchor" : "start", "fill" : "#222222"}).animate({"opacity" : 1}, 500, ">").toFront();
   }
 }
 
